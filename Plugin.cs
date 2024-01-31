f0da4d3102d7dde91421c1bad42db6e4915d3b99
reedybee <Reed@DESKTOP-2O50SCE>
2024-01-30 22:56:12 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿// system
+using System.Collections;
+// bepinex
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+// harmony
+using HarmonyLib;
+// unity
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// reedybee
+using LCRevolverMod.Scripts;
+
+namespace LCRevolverMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class RevolverModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "reedybee.LCRevolverMod";
+        private const string modName = "LC Revolver Mod";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static RevolverModBase instance;
+
+        public static ManualLogSource log;
+        // called once upon game startup
+        public void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            log = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            log.LogInfo($"{modName} has awoken.");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+
+            harmony.PatchAll();
+;        }
+        // callback when new scene loads
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
+        {
+            log.LogMessage("Scene Loaded " + scene.name + " with " + mode + " mode.");
+            log.LogMessage(scene.name);
+            if (scene.name == "SampleSceneRelay")
+            {
+
+            }
+        }
+    }
+} 
---
+++ Scripts/RevolverAmmo.cs
@@ -0,0 +1,21 @@
+﻿// system
+using System.Collections;
+// bepinex
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+// harmony
+using HarmonyLib;
+// unity
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// reedybee
+using LCRevolverMod.Scripts;
+namespace LCRevolverMod.Scripts
+{
+    internal class RevolverAmmo : GrabbableObject
+    {
+
+    }
+}
